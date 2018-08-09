521e7d094bd282e5ca8addb887b8f01d229e1b97
rafaelmotaalves <rma7@cin.ufpe.br>
2018-08-09 13:58:41 -0300

    bb

--- controller/project.js
+++ controller/project.js
@@ -5,12 +5,29 @@ module.exports = {
     getProjects: async (req, res) => {
         try {
             const params = {
-                categories: { $in: req.body.tags, },
+                categories: { $in: mongoose.Types.Schema.ObjectId(req.body.tags), },
             }
             const foundProjects = await Project.find({params});
-            res.json(foundProjects);
+            return res.json(foundProjects);
         } catch (err) {
             console.log(err);
         }
     },
+
+    createProject: async (req, res) => {
+        try {
+            const data = {
+                name: req.body.name,
+                description: req.body.description,
+                type: req.body.type,
+                tasks: req.body.tasks.map(task => ({ ...task, _id: mongoose.Types.Schema.ObjectId() })), 
+            }
+            const createdProjet = await Project.create(data);
+            return res.json(createdProjet);
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
+
 }
